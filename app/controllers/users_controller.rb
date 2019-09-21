5be9c744b19760d9a0a0eadcba06d6bd6f08194f
a-2141026 <cmdhs1025@gmail.com>
2019-09-21 16:24:06 +0900

    シンボル変更、current.userを含めない

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,11 @@
 class UsersController < ApplicationController
 
   def index
-    @users = User.where('name LIKE(?)', "%#{params[:keyword]}%").limit(10)
+    @users = User.where('name LIKE(?)', "%#{params[:name]}%" ).where("id NOT IN (#{current_user.id})")
     respond_to do |format|
-    format.json
-    end   
+      format.html
+      format.json
+    end
   end
 
   def edit
