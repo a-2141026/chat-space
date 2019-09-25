ea8126b7fddada9193fe43cfe9dc028a9272eccc
a-2141026 <cmdhs1025@gmail.com>
2019-09-25 17:42:05 +0900

    Update messages.rb

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -1,9 +1,8 @@
 FactoryBot.define do
-  factory :user do
-    password = Faker::Internet.password(8)
-    name {Faker::Name.last_name}
-    email {Faker::Internet.free_email}
-    password {password}
-    password_confirmation {password}
+  factory :message do
+    content {Faker::Lorem.sentence}
+    image {File.open("#{Rails.root}/public/images/no_image.jpg")}
+    user
+    group
   end
 end
